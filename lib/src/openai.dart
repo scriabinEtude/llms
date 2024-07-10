786abd017905383d8f51d200dbd09a9766b1f908
임한결 <scriabinetude87@gmail.com>
2024-07-10 16:08:12 +0900

    Thread Service Init

--- lib/src/openai.dart
+++ lib/src/openai.dart
@@ -3,17 +3,20 @@ import 'package:llms/src/openai/service/chat/chat.dart';
 import 'package:llms/src/openai/service/file/file.dart';
 import 'package:llms/src/openai/service/image/image.dart';
 import 'package:llms/src/openai/service/models/models.dart';
+import 'package:llms/src/openai/service/thread/thread.dart';
 
 class OpenAI {
   final OpenAIModelsService models;
   final OpenAIChatService chat;
   final OpenAIImageService image;
   final OpenAIFileService file;
+  final OpenAIThreadService thread;
 
   OpenAI({
     required OpenAIClient client,
   })  : models = OpenAIModelsService(client: client),
         chat = OpenAIChatService(client: client),
         image = OpenAIImageService(client: client),
-        file = OpenAIFileService(client: client);
+        file = OpenAIFileService(client: client),
+        thread = OpenAIThreadService(client: client);
 }
---
+++ lib/src/openai/service/thread/thread.dart
@@ -0,0 +1,7 @@
+import 'package:llms/src/client/client.dart';
+
+class OpenAIThreadService {
+  final OpenAIClient client;
+
+  const OpenAIThreadService({required this.client});
+}
